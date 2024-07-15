da48076e66b322b1916bbd2f2c7a8360c0d0434b
Zahran03 <zalfarizi502@gmail.com>
2024-07-16 06:03:02 +0700

    commit bagian 2 zahran

--- app/src/main/java/com/example/newsapp/ui/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/ui/NewsActivity.kt
@@ -1,10 +1,7 @@
-package com.example.newsapp.ui
+package com.example.newsapp.ui.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.newsapp.R
 
 class NewsActivity : AppCompatActivity() {
