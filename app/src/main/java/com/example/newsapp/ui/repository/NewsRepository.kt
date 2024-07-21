0afbeaaa908882f6e97699e85b7efee63c783324
RoyhanNabilli <royhannabilli@gmail.com>
2024-07-21 16:04:06 +0700

    part 4 adit dan roy

---
+++ app/src/main/java/com/example/newsapp/ui/repository/NewsRepository.kt
@@ -0,0 +1,20 @@
+package com.example.thenewsapp.repository
+
+import com.example.newsapp.ui.api.RetrofitInstance
+import com.example.newsapp.ui.db.ArticleDatabase
+import com.example.newsapp.ui.models.Article
+
+class NewsRepository(val db: ArticleDatabase) {
+
+    suspend fun getHeadlines(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getHeadLines(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getFavouriteNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
+}
