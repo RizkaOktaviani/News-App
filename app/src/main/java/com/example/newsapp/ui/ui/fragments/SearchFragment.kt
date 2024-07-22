2b847c67c8b9f7281c25bcfff228e52d2d68afe9
Zahran03 <zalfarizi502@gmail.com>
2024-07-22 08:55:20 +0700

    part final

--- app/src/main/java/com/example/newsapp/ui/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/ui/fragments/SearchFragment.kt
@@ -10,11 +10,5 @@ import com.example.newsapp.R
 
 class SearchFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-    }
+
 }
